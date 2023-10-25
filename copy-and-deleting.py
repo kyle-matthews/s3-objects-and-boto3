574085d4c4eead72dc86cc5e53b27fc7de058a16
Kyle Matthews <linescrawler@gmail.com>
2023-10-25 21:20:17 +0100

    Adds error handling to script.

--- copy-and-deleting.py
+++ copy-and-deleting.py
@@ -1,4 +1,14 @@
 import boto3
 
-s3 = boto3.client('s3')
-s3.copy_object(Bucket='my-boto3-bucket-lola', Key='fried/bucket-chicken.webp', CopySource='my-bucket-of-chicken/fried/bucket-chicken.webp')
+try:
+    s3 = boto3.client('s3')
+    s3.copy_object(Bucket='my-boto3-bucket-lola', Key='fried/bucket-chicken.webp', CopySource='my-bucket-of-chicken/fried/bucket-chicken.webp')
+
+except NoCredentialsError:
+    print('AWS credentials not found, please try again.')
+
+except ClientError:
+    if ClientError.response['Error']['Code'] == 'NoSuchBucket':
+            print('The specified bucket does not exist.')
+    else:
+        print('An error occured:', e)
