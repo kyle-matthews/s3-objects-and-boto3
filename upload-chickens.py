e0f06dc82304ca9ea31a0e375a7b97ba55423cbd
Kyle Matthews <linescrawler@gmail.com>
2023-10-25 21:12:33 +0100

    Adds error handling to upload script.

--- upload-chickens.py
+++ upload-chickens.py
@@ -1,4 +1,13 @@
 import boto3
 
-s3 = boto3.client('s3')
-s3.upload_file('/Users/kylematthews/bucket-chicken.webp', 'my-bucket-of-chicken', 'fried/bucket-chicken.webp')
+try:
+    s3 = boto3.client('s3')
+    s3.upload_file('/Users/kylematthews/bucket-chicken.webp', 'my-bucket-of-chicken', 'fried/bucket-chicken.webp')
+    print('File uploaded successfully.')
+except NoCredentialsError:
+    print('Incorrect AWS credentials. Please configure and try again.')
+except ClientError:
+    if ClientError.response['Error']['Code'] == 'NoSuchBucket':
+            print('The specified bucket does not exist.')
+    else:
+        print('An error occured:', e)
